a107a91ca0df43342f4df585503a288f013a1383
theek23 <theekshanadesilva28@gmail.com>
2024-05-19 19:10:59 +0530

    Employee form update

--- hello-shoes-backend/src/main/java/lk/ijse/gdse66/helloshoes/backend/service/SupplierService.java
+++ hello-shoes-backend/src/main/java/lk/ijse/gdse66/helloshoes/backend/service/SupplierService.java
@@ -1,6 +1,5 @@
 package lk.ijse.gdse66.helloshoes.backend.service;
 
-import lk.ijse.gdse66.helloshoes.backend.dto.CustomerDTO;
 import lk.ijse.gdse66.helloshoes.backend.dto.SupplierDTO;
 
 import java.util.List;
